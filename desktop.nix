eaeca80527cb553532aa8dcc5e4f4ad3e6721f77
idesyatov <regull@bk.ru>
2020-06-27 00:03:16 +0300

    test i3config

--- desktop.nix
+++ desktop.nix
@@ -4,11 +4,6 @@
 let
   nonfree = import <nixos> { config.allowUnfree = true; };  
 in {
-  imports = 
-    [
-      ./configs/i3.nix
-    ];  
-
   environment.pathsToLink = [ "/libexec" ];
 
   # Enable the X11 windowing system with i3wm 
@@ -71,15 +66,84 @@ in {
         ''}"
     '';
 
+    # i3wm settings
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
         dmenu
         networkmanagerapplet
         i3status i3lock i3blocks
-        
-        compton
       ];
+      
+      # i3 CONFIG
+      config = {
+        modifier = "Mod4";
+        fonts = [ "pango:monospace 8" ];
+
+        keybindings = let mod = config.xsession.windowManager.i3.config.modifier; in lib.mkOptionDefault {
+          "${mod}+q" = "kill";
+          
+          "${mod}+Shift+c" = "reload";
+          "${mod}+Shift+r" = "restart";
+          "${mod}+s" = "layout stacking";
+          "${mod}+w" = "layout tabbed";
+          "${mod}+e" = "layout toggle split";
+          "${mod}+r" = "mode resize";
+
+          "${mod}+f" = "fullscreen toggle";
+          "${mod}+Shift+space" = "floating toggle";
+
+          "${mod}+Left" = "focus left";
+          "${mod}+Down" = "focus down";
+          "${mod}+Up" = "focus up";
+          "${mod}+Right" = "focus right";
+
+          "${mod}+Shift+Left" = "move left";
+          "${mod}+Shift+Down" = "move down";
+          "${mod}+Shift+Up" = "move up";
+          "${mod}+Shift+Right" = "move right";
+
+          "${mod}+v" = "split v";
+          "${mod}+h" = "split h";
+
+          "${mod}+1" = "workspace 1";
+          "${mod}+2" = "workspace 2";
+          "${mod}+3" = "workspace 3";
+          "${mod}+4" = "workspace 4";
+          "${mod}+5" = "workspace 5";
+          "${mod}+6" = "workspace 6";
+          "${mod}+7" = "workspace 7";
+          "${mod}+8" = "workspace 8";
+          "${mod}+9" = "workspace 9";
+          "${mod}+0" = "workspace 10";
+
+          "${mod}+Shift+1" = "move container to workspace 1";
+          "${mod}+Shift+2" = "move container to workspace 2";
+          "${mod}+Shift+3" = "move container to workspace 3";
+          "${mod}+Shift+4" = "move container to workspace 4";
+          "${mod}+Shift+5" = "move container to workspace 5";
+          "${mod}+Shift+6" = "move container to workspace 6";
+          "${mod}+Shift+7" = "move container to workspace 7";
+          "${mod}+Shift+8" = "move container to workspace 8";
+          "${mod}+Shift+9" = "move container to workspace 9";
+          "${mod}+Shift+0" = "move container to workspace 10";
+
+          # brightness
+          XF86MonBrightnessDown = "exec ${pkgs.brightnessctl}/bin/brightnessctl -q s 5%-";
+          XF86MonBrightnessUp = "exec ${pkgs.brightnessctl}/bin/brightnessctl -q s 5%+";
+
+          # volume
+          XF86AudioRaiseVolume = "exec ${pkgs.ponymix}/bin/ponymix increase 5";
+          XF86AudioLowerVolume = "exec ${pkgs.ponymix}/bin/ponymix decrease 5";
+          XF86AudioMute = "exec ${pkgs.ponymix}/bin/ponymix toggle";
+
+          # media
+          XF86AudioPlay = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+          XF86AudioPause = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+          XF86AudioNext = "exec ${pkgs.playerctl}/bin/playerctl next";
+          XF86AudioPrev = "exec ${pkgs.playerctl}/bin/playerctl previous";
+        };
+      };
     };
   };
 
