6d63bd11698a20fb6a1ad05def20635ec6cc1dd8
idesyatov <regull@bk.ru>
2020-06-27 00:32:37 +0300

    test i3config

--- configs/i3.nix
+++ configs/i3.nix
@@ -1,48 +1,106 @@
-{ config, lib, pkgs, ...}:
+{ pkgs, lib, config, ... }:
 
-let
-  mod = "Mod4";
-in {
-  
-  xsession.windowManager.i3 = {
+{
+  xsession = {
     enable = true;
-    config = {
-      modifier = mod;
+    windowManager.i3 = {
+      enable = true;
+      
+      config = {
+        keybindings = let mod = config.xsession.windowManager.i3.config.modifier; in lib.mkOptionDefault {
+          "${mod}+Return" = "exec kitty";
+          "${mod}+q" = "kill";
+          "${mod}+d" = "exec ${pkgs.bash}/bin/bash /etc/nixos/home/dotfiles/rofi-menus/appsmenu.sh";
+          "${mod}+i" = "exec ${pkgs.bash}/bin/bash /etc/nixos/home/dotfiles/rofi-menus/i3keybindingsmenu.sh";
+          "${mod}+o" = "exec ${pkgs.bash}/bin/bash /etc/nixos/home/dotfiles/rofi-menus/powermenu.sh";
+          "${mod}+Shift+s" = "exec ${pkgs.bash}/bin/bash /etc/nixos/home/dotfiles/rofi-menus/scrotmenu.sh";
+          "${mod}+Shift+a" = "exec ${pkgs.bash}/bin/bash /etc/nixos/home/dotfiles/rofi-menus/mpdmenu.sh";
+          "${mod}+c" = "exec ${pkgs.chromium}/bin/chromium";
+          "${mod}+n" = "exec ${pkgs.gnome3.nautilus}/bin/nautilus";
+          "${mod}+p" = "exec ${pkgs.betterlockscreen}/bin/betterlockscreen --lock";
+          "${mod}+g" = "gaps inner current plus 5";
+          "${mod}+Shift+g" = "gaps inner current minus 5";
+          "${mod}+m" = "exec kill -s USR1 $(pidof deadd-notification-center)";
+          
+          "${mod}+Shift+c" = "reload";
+          "${mod}+Shift+r" = "restart";
+          "${mod}+s" = "layout stacking";
+          "${mod}+w" = "layout tabbed";
+          "${mod}+e" = "layout toggle split";
+          "${mod}+r" = "mode resize";
 
-      #fonts = ["DejaVu Sans Mono, FontAwesome 6"];
+          "${mod}+f" = "fullscreen toggle";
+          "${mod}+Shift+space" = "floating toggle";
 
-      keybindings = lib.mkOptionDefault {
-        "${mod}+p" = "exec ${pkgs.dmenu}/bin/dmenu_run";
-        "${mod}+x" = "exec screenshot";
-        "${mod}+Ctrl+x" = "exec screenshot-to-zettelkasten";
-        "${mod}+Shift+x" = "exec lockscreen";
+          "${mod}+Left" = "focus left";
+          "${mod}+Down" = "focus down";
+          "${mod}+Up" = "focus up";
+          "${mod}+Right" = "focus right";
 
-        # Focus
-        "${mod}+j" = "focus left";
-        "${mod}+k" = "focus down";
-        "${mod}+l" = "focus up";
-        "${mod}+semicolon" = "focus right";
+          "${mod}+Shift+Left" = "move left";
+          "${mod}+Shift+Down" = "move down";
+          "${mod}+Shift+Up" = "move up";
+          "${mod}+Shift+Right" = "move right";
 
-        # Move
-        "${mod}+Shift+j" = "move left";
-        "${mod}+Shift+k" = "move down";
-        "${mod}+Shift+l" = "move up";
-        "${mod}+Shift+semicolon" = "move right";
+          "${mod}+v" = "split v";
+          "${mod}+h" = "split h";
 
-        # My multi monitor setup
-        "${mod}+m" = "move workspace to output DP-2";
-        "${mod}+Shift+m" = "move workspace to output DP-5";
+          "${mod}+1" = "workspace 1";
+          "${mod}+2" = "workspace 2";
+          "${mod}+3" = "workspace 3";
+          "${mod}+4" = "workspace 4";
+          "${mod}+5" = "workspace 5";
+          "${mod}+6" = "workspace 6";
+          "${mod}+7" = "workspace 7";
+          "${mod}+8" = "workspace 8";
+          "${mod}+9" = "workspace 9";
+          "${mod}+0" = "workspace 10";
 
-        # Quick floating windows
-        "${mod}+Shift+p" = "exec myst -c floating -g=150x50 tmux new-session -s floating ~/code/self/bin/open-zettel";
+          "${mod}+Shift+1" = "move container to workspace 1";
+          "${mod}+Shift+2" = "move container to workspace 2";
+          "${mod}+Shift+3" = "move container to workspace 3";
+          "${mod}+Shift+4" = "move container to workspace 4";
+          "${mod}+Shift+5" = "move container to workspace 5";
+          "${mod}+Shift+6" = "move container to workspace 6";
+          "${mod}+Shift+7" = "move container to workspace 7";
+          "${mod}+Shift+8" = "move container to workspace 8";
+          "${mod}+Shift+9" = "move container to workspace 9";
+          "${mod}+Shift+0" = "move container to workspace 10";
+
+          # brightness
+          XF86MonBrightnessDown = "exec ${pkgs.brightnessctl}/bin/brightnessctl -q s 5%-";
+          XF86MonBrightnessUp = "exec ${pkgs.brightnessctl}/bin/brightnessctl -q s 5%+";
+
+          # volume
+          XF86AudioRaiseVolume = "exec ${pkgs.ponymix}/bin/ponymix increase 5";
+          XF86AudioLowerVolume = "exec ${pkgs.ponymix}/bin/ponymix decrease 5";
+          XF86AudioMute = "exec ${pkgs.ponymix}/bin/ponymix toggle";
+
+          # media
+          XF86AudioPlay = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+          XF86AudioPause = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+          XF86AudioNext = "exec ${pkgs.playerctl}/bin/playerctl next";
+          XF86AudioPrev = "exec ${pkgs.playerctl}/bin/playerctl previous";
+        };
+        modes = {
+          resize = { 
+            Down = "resize grow height 10 px or 10 ppt"; 
+            Left = "resize shrink width 10 px or 10 ppt"; 
+            Right = "resize grow width 10 px or 10 ppt"; 
+            Up = "resize shrink height 10 px or 10 ppt"; 
+
+            Return = "mode default";
+            Escape = "mode default"; 
+            "${config.xsession.windowManager.i3.config.modifier}+r" = "mode default";
+          };
+        };
+        startup = [
+          {command = "${pkgs.feh}/bin/feh --bg-fill /etc/nixos/.background-image wallpaper.png"; always = true;}
+        ];
+        terminal = "rxvt";
+        modifier = "Mod4";
+        workspaceAutoBackAndForth = true;
       };
-
-      bars = [
-        {
-          position = "bottom";
-          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${./i3status-rust.toml}";
-        }
-      ];
     };
   };
 }
